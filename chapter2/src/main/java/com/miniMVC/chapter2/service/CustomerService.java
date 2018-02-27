c7de416ba2ede4639787abda9ce28d5869854844
yangjiaqi <yjq14114@gmail.com>
2018-02-27 17:53:40 +0800

    jdbc driver

--- chapter2/src/main/java/com/miniMVC/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/com/miniMVC/chapter2/service/CustomerService.java
@@ -1,14 +1,36 @@
 package com.miniMVC.chapter2.service;
 
 import com.miniMVC.chapter2.model.Customer;
+import com.miniMVC.commons.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by yjq14 on 2018/2/26.
  */
 public class CustomerService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver", e);
+        }
+    }
     /**
      * 获取客户列表
      * @param keyword
