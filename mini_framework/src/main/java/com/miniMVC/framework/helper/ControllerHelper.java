5edc9396484c2797b672cc13da378f321f0b466d
yangjq <yjq14114@gmail.com>
2018-03-04 01:03:03 +0800

    初始化controller

--- mini_framework/src/main/java/com/miniMVC/framework/helper/ControllerHelper.java
+++ mini_framework/src/main/java/com/miniMVC/framework/helper/ControllerHelper.java
@@ -1,7 +1,12 @@
 package com.miniMVC.framework.helper;
 
+import com.miniMVC.commons.CollectionUtil;
+import com.miniMVC.framework.annotation.Action;
+import com.miniMVC.framework.bean.Handler;
 import com.miniMVC.framework.bean.Request;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -10,10 +15,39 @@ import java.util.Set;
  * Created by yjq14 on 2018/3/4.
  */
 public class ControllerHelper {
-    private static final Map<Request, Object> ACTION_MAP = new HashMap<>();
+    private static final Map<Request, Handler> ACTION_MAP = new HashMap<>();
 
     static {
-        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        Set<Class<?>> controllerClassSet = ClassHelper.getControllerClassSet();
+        if (CollectionUtil.isNotEmpty(controllerClassSet)) {
+            for (Class<?> controllerClass :
+                    controllerClassSet) {
+                Method[] methods = controllerClass.getDeclaredMethods();
+                if (ArrayUtils.isNotEmpty(methods)) {
+                    for (Method method :
+                            methods) {
+                        if (method.isAnnotationPresent(Action.class)) {
+                            Action action = method.getAnnotation(Action.class);
+                            String mapping = action.value();
+                            if (mapping.matches("\\w+:/\\w")) {
+                                String[] array = mapping.split(":");
+                                if (ArrayUtils.isNotEmpty(array) && array.length == 2) {
+                                    String requestMethod = array[0];
+                                    String requestPath = array[1];
+                                    Request request = new Request(requestMethod, requestPath);
+                                    Handler handler = new Handler(controllerClass, method);
+                                    ACTION_MAP.put(request, handler);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
+    public static Handler getHandler(String requestMethod, String requestPath) {
+        Request request = new Request(requestMethod, requestPath);
+        return ACTION_MAP.get(request);
     }
 }
