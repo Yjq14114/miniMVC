00db8416c8ddeab8f6844aa9edc1b87c20f165c4
yangjiaqi <yang.jiaqi@vpclub.cn>
2018-03-12 00:16:34 +0800

    动态代理

--- chapter4/src/main/java/com/miniMvc/proxy/CGLibProxy.java
+++ chapter4/src/main/java/com/miniMvc/proxy/CGLibProxy.java
@@ -1,17 +1,43 @@
 package com.miniMvc.proxy;
 
+import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
-import java.lang.invoke.MethodHandleInfo;
 import java.lang.reflect.Method;
 
 /**
  * Created by yjq14 on 2018/3/11.
  */
 public class CGLibProxy implements MethodInterceptor{
+
+    private static CGLibProxy instance = new CGLibProxy();
+
+    private CGLibProxy() {
+
+    }
+    public static CGLibProxy getInstance() {
+        return instance;
+    }
+    public <T> T getProxy(Class<T> cls){
+        return (T) Enhancer.create(cls, this);
+    }
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        return null;
+        before();
+        Object result = methodProxy.invokeSuper(o, objects);
+        after();
+        return result;
+    }
+    private void before() {
+        System.out.println("before");
+    }
+    private void after() {
+        System.out.println("after");
+    }
+
+    public static void main(String[] args) {
+        StaticHello hello = CGLibProxy.getInstance().getProxy(StaticHelloImpl.class);
+        hello.say("jack");
     }
 }
